0dc216c8027bb1c47cfaa7b46c4580be6492631b
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-07-08 23:52:20 +0530

    Week1Day3
    Week1Day3

---
+++ Week1Day3/function.js
@@ -0,0 +1,14 @@
+//function->  A function is a reusable block of code that performs a specific task 
+// when it is called.
+
+function greet(a,b) {  //parameters
+//let a=3
+//let b=5
+console.log(a+b);  //function body
+}
+greet(2,4)
+greet(10,20)
+greet(2,3)//Arguments
+
+
+
